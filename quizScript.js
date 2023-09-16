2d634e6cdee504a48c3c6f92d6d2520fd2c4e6f1
aaroncolaco <ac121654@avcol.school.nz>
2023-09-17 00:26:17 +1200

    I fixed the spelling errors in some of my comments

--- quizScript.js
+++ quizScript.js
@@ -1,5 +1,5 @@
       
-      //use queery selctor to get Elements from html by id or class assings them to vairbles.
+      //use query selector to get Elements from html by id or class assigns them to variables.
       let userName = document.querySelector("#userName");
       let welcomeMessage = document.querySelector("#welcomeMessage");
       let nameInputBox = document.querySelector(".nameInputBox");
@@ -24,7 +24,7 @@
 
       //i keeps track of question numbers 
       let i = 0;
-      //sets points varibale to 0
+      //sets points variable to 0
       var points = 0;
 
       // 3 arrays empty arrays that will store question answers and options.
@@ -36,21 +36,21 @@
 
       //function to Validate Name
       function getValidatedName() {
-      // if the user name is not vaild alert user
+      // if the user name is not valid alert user
         if (userName.value.length < 1 || userName.value.length > 30) {
           alert("Please enter a name between 1-30 characters to begin");
         } 
-    // if the above condition is no meet the users name is viald wellcoem the user to the quiz by changing the inner html of the welcome message vairbale.
+    // if the above condition is no meet the users name is valid welcome the user to the quiz by changing the inner html of the welcome message variable.
         else {
           welcomeMessage.innerHTML = "Welcome " + userName.value + ", to this Maui and the Sun Maori Quiz";
           nameInputBox.style.display = "none";
-          levelBox.style.display = "block"; // makes the levle box visable
+          levelBox.style.display = "block"; // makes the level box variable
         }
       }
 // quiz method take in question options and answer arrays.
       function quiz(qArray, oArray, aArray) {
-        quizbox.style.display = "block"; // makes the quiz box visable
-        //asigns the question answer and options that get passed into the quiz mehtod to the mepty arrasy create at the top.
+        quizbox.style.display = "block"; // makes the quiz box visible
+        //asigns the question answer and options that get passed into the quiz method to the empty array created near the top of the js file.
         questionsArray = qArray;
         optionsArray = oArray;
         answersArray = aArray;
@@ -63,7 +63,7 @@
       function displayQuestion() {
         // display current question using the i index of the question array.
         question.innerHTML = questionsArray[i];
-        //display the option 1 2 3 and 4 using a 2 dimensional array stutuce. i is the current question, i0 is option 1 for the current question i1 is option 2 for the current question i2 is option 3 for  question i.
+        //display the option 1 2 3 and 4 using a 2 dimensional array structure. i is the current question, i0 is option 1 for the current question i1 is option 2 for the current question i2 is option 3 for  question i.
         option1.innerHTML = optionsArray[i][0];
         option2.innerHTML = optionsArray[i][1];
         option3.innerHTML = optionsArray[i][2];
@@ -74,33 +74,33 @@
         optionsButtons.style.display = "block";// display the option button
       }
 
-      // This function is called when the user click on a option button. The option button will paass a number into the check answer function. 
+      // This function is called when the user click on a option button. The option button will passes a number into the check answer function. 
      
       function checkAnswer(selectedOption) {
-        //make the next button visbale and hides the option buttons.
+        //make the next button visible and hides the option buttons.
         nextButton.style.display = "block";
         optionsButtons.style.display = "none";
-        // store the vlaue passed in to the method as a avribale called user chocie
+        // store the value passed in to the method as a variable called user choice
         const userChoice = selectedOption;
-        //store the current correct answer at the current i postion as a vairbale called correctAnswer.
+        //store the current correct answer at the current i position as a variable called correctAnswer.
         const correctAnswer = answersArray[i];
-        // checks if the user answer matchs the correct answer
-        // if the user answer is right increase the points vairble by 1
+        // checks if the user answer matches the correct answer
+        // if the user answer is right increase the points variable by 1
         if (userChoice == correctAnswer) {
           points++;
-          // set the score inner html vlaue to the vlaue of points. 
+          // set the score inner html value to the value of points. 
           score.innerHTML = points;
-          feedback.innerHTML = "Correct!"; // set the feebakc inner html vlaue to correct
-          // set the colour of feed back text to green and add a green box shaddow arrounf the quiz continaer.
+          feedback.innerHTML = "Correct!"; // set the feedback inner html value to correct
+          // set the colour of feed back text to green and add a green box shadow arround the quiz container.
           feedback.style.color = "green"; 
           quizContent.style.boxShadow = "0 0 50px #00FF00";
-// if the naswer is woenrg 
+// if the answer is wrong 
         } else {
        // Display the correct answer in the 'feedback' inner HTML. Get the correct answer 
        //from the  current option array [i][correct answer number] position. Answers in my quiz are store as  1,2,3,4 and the option array is 0,1,2,3, we adjust by subtracting one to match array postioning and diplsay the correct answer.
 
-          feedback.innerHTML = " Your answer waas Incorrect! The correct answer was: " + optionsArray[i][(correctAnswer - 1)];
-          // set colour of ffebakc text to red and adds a red box shaddow.
+          feedback.innerHTML = " Your answer was Incorrect! The correct answer was: " + optionsArray[i][(correctAnswer - 1)];
+          // set colour of feedback text to red and adds a red box shadow.
           feedback.style.color = "red";
           quizContent.style.boxShadow = "0 0 50px #FF0000";
         }
@@ -108,13 +108,13 @@
       }
       // this function is called when the user clicks the next button 
       function nextQuestion() {
-        //rest the box sahddow 
+        //rest the box shadows 
         quizContent.style.boxShadow = "";
-        i++;// increase i hence increased the quesiton number by 1
+        i++;// increase i hence increased the question number by 1
         
         nextButton.style.display = "none"; // hides the next button 
        
-        // if i is less the lenght of questions in the question array call dipslay question function if it is not called endOfQuiz function.
+        // if i is less the length of questions in the question array call display question function if it is not called endOfQuiz function.
         if (i < questionsArray.length) {
 
           displayQuestion();
@@ -122,35 +122,35 @@
         } else {
           endOfQuiz();
         }
-        feedback.innerHTML = ""; // rest feedback messgae  after each qeustion.
+        feedback.innerHTML = ""; // rest feedback message  after each question.
 
       }
 
-      // This fucntion is called at the end of the quiz. 
+      // This Function is called at the end of the quiz. 
       function endOfQuiz() {
         quizBox.style.display ="none"; // hide the quiz box
-        progrss.style.display = "none";// hide the progess bar
-        // displays a final scorre message by dipslaying the name and the users points. 
+        progrss.style.display = "none";// hide the progress bar
+        // displays a final score message by displaying the name and the users points. 
         finalScoreMessage.innerHTML = "Quiz completed " + userName.value + " ,you got " + points + " out of " + questionsArray.length;
         percentage = Math.round((points / answersArray.length) * 100); // create a percentage score
-        // set the precnettage text inner html to the calclated score.  
+        // set the percentage text inner html to the calculated score.  
         percentageText.innerHTML = percentage + "%";
         replay.style.display = "block";  // display the replay menu 
 
 
-// checks if the user percantge is above 30 and changes the perozionlaized messgae innenr html to a peroxozniled message including the user name and their points. Also diplsay hwo much need for a perfect score. 
+// checks if the user percentage is above 30 and changes the perozionlaized message inner html to a personalized message including the user name and their points. Also diplsay hwo much need for a perfect score. 
  if (percentage > 30) {
   personalizedMessage.innerHTML = userName.value + " that's a good score. Have another go; you are only " + (questionsArray.length - points) + " correct answer(s) away  away from a perfect score. Consider doing some research about the Maori culture or check out the 'About' section to learn more.";
     personalizedMessage.style.color ="green"// sets the colour of the text to green 
 }
 
-// checks if the user percantge is 100% and changes the perozionlaized messgae innenr html to a peroxozniled message including the user name and their points. 
+// checks if the user percentage is 100% and changes the perozionlaized message inner html to a perosozniled message including the user name and their points. 
 if (percentage === 100) {
   personalizedMessage.innerHTML = userName.value + " that's a perfect score. Tino Pai";
   personalizedMessage.style.color ="green"// sets the colour of the text to green 
 }
 
-// checks if the user percantge is less then 30 and  then changes the perozionlaized messgae innenr html to a peroxozniled message including the user name and their points. Also makes the font red.
+// checks if the user percantage is less then 30 and  then changes the perozionlaized message innenr html to a perosozniled message including the user name and their points. Also makes the font red.
 if (percentage <= 30) {
   personalizedMessage.innerHTML = userName.value + " Good attempt; however, you can do better. Consider checking out the 'About' page or doing some more research about Maori Culture.";
   personalizedMessage.style.color ="red"// sets the colour of the text to red
@@ -158,17 +158,17 @@ if (percentage <= 30) {
  
 
       }
-    // fucntion called wehn user clcik laod menu again 
+    // Function called when user clicks load menu again 
       function loadMenu() {
         levelBox.style.display = "block"; // display the level box
-        welcomeMessage.innerHTML = "Welcome Back " + userName.value; // display a wleocme back message by changing the inner html of the welcome message.
-       //makes the porgess and quiz box visbale and also hides the replay menu 
+        welcomeMessage.innerHTML = "Welcome Back " + userName.value; // display a welcome back message by changing the inner html of the welcome message.
+       //makes the progess and quiz box variable and also hides the replay menu 
         progrss.style.display = "block"; 
-        quizbox.style.display = "blcok";
+        quizbox.style.display = "block";
         replay.style.display = "none";
         points = 0; // rests points
         score.innerHTML = points;
-        i = 0; // rests question number 
+        i = 0; // resets question number 
         
       }
      
@@ -232,7 +232,7 @@ if (percentage <= 30) {
           ["Hopu", "Hopa", "Hopue", "Hopu"]
     
         ];
-       //pass the above emdium level  arrays in to the quiz method 
+       //pass the above medium level arrays in to the quiz method 
         quiz(mediumQuestions, mediumOptions, mediumAnswers);
       }
 
@@ -260,6 +260,6 @@ if (percentage <= 30) {
       const hardAnswers = [
         1,2,2,1,1,2,1
       ];
-       //pass the above hard arrays in to the quiz method 
+       //pass the above hard level arrays in to the quiz method 
      quiz(hardQuestions,hardOptions,hardAnswers)
     }
